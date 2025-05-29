6b6a9e53a45ff89ce9a2acf79224a402aab99e8b
obedflr <obedflr@gmail.com>
2025-05-29 20:37:15 +0200

    my portfolio

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+const cambiarTraduccion = document.getElementById("cambiarTraduccion");
+ 
+ 
+let translations = {};
+let idiomaActual = "es";
+ 
+fetch('translations/translations.json')
+    .then(response => response.json())
+    .then(data => {
+        translations = data;
+        setLanguage('es');
+    });
+ 
+function updateTexts(lang) {
+    const elements = Object.keys(translations[lang]);
+    elements.forEach(key => {
+        const el = document.getElementById(key);
+        if (el) el.textContent = translations[lang][key];
+    });
+}
+ 
+function setLanguage(lang) {
+    idiomaActual = lang;
+    if (!translations[lang]) return;
+    updateTexts(lang);
+}
+ 
+cambiarTraduccion.addEventListener("click", ()=>{
+    if(idiomaActual==="es"){
+        setLanguage("en");
+ 
+    }else setLanguage("es")
+});
